68babfb69cfe2334594135d941ce0074eccd4e42
kamran <mma744237@gmail.com>
2023-10-08 00:46:21 +0600

    thard commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,33 @@ barBtn.addEventListener('click', toggleNav);
 function toggleNav() {
   navbar.classList.toggle('active-nav')
 }
+
+
+var videoPlayer = document.getElementById('videoPlayer');
+var myVideo = document.getElementById('myVideo');
+var closebtn = document.getElementsByClassName('close-btn');
+function stopvideo() {
+  videoPlayer.style.display = "none";
+}
+function playVideo(file) {
+  myVideo.src = file;
+  videoPlayer.style.display = "block";
+}
+
+
+var swiper = new Swiper(".mySlide", {
+  slidesPerView: 6,
+  spaceBetween: 60,
+  // grabCursor: true,
+  preview: 6,
+  loop:true,
+  freeMode: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+
+
+
